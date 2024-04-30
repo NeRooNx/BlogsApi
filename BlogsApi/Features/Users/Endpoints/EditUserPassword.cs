86d7f2933b07debcaca91d0f3ab57bd05c499ac5
NeRooNx <neroonsk8@gmail.com>
2024-05-01 01:17:58 +0200

    edit current user password

---
+++ BlogsApi/Features/Users/Endpoints/EditUserPassword.cs
@@ -0,0 +1,81 @@
+﻿using BlogsApi.Extensions;
+using BlogsApi.Features.Authentication.Service;
+using BlogsApi.Infrastructure;
+using BlogsApi.Shared;
+using BlogsModel.Models;
+using FluentValidation;
+using Immediate.Apis.Shared;
+using Immediate.Handlers.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogsApi.Features.Endpoints.Users;
+
+[Handler]
+[MapPut("api/v1/users/edit/password")]
+[Authorize]
+public partial class EditUserPassword
+{
+
+    internal static Results<Ok<Response>, BadRequest<Error>> TransformResult(Result<Response> result)
+    {
+        return result.IsFailure
+            ? TypedResults.BadRequest(result.Error)
+            : TypedResults.Ok(result.Value);
+    }
+
+    public class Request
+    {
+        public string? Password { get; set; }
+    }
+
+
+    public class Response
+    {
+    }
+
+    private static async ValueTask<Result<Response>> Handle(
+        Request request, 
+        BlogsDBContext dBContext, 
+        CurrentUser currentUser,
+        IValidator<Request> validator, 
+        CancellationToken cancellationToken)
+    {
+        var validationResult = validator.Validate(request);
+
+        if (!validationResult.IsValid)
+        {
+            return Result.Failure<Response>(new Error(
+                "EditUserPassword.Validation",
+                validationResult.ToString()));
+        }
+
+        User? user = await dBContext.Users.SingleOrDefaultAsync(x => x.Id == currentUser.Id, cancellationToken: cancellationToken);
+
+        if (user is null)
+        {
+            return Result.Failure<Response>(new("EditUserPassword.Handle", "El User no existe."));
+        }
+
+        user.Password = BCryptHelper.EncryptPassword(request.Password!);
+
+        await dBContext.SaveChangesAsync(cancellationToken);
+
+        return new Response();
+    }
+
+    public class Validator : AbstractValidator<Request>
+    {
+        public const string ClassName = nameof(EditUser);
+        public Validator() 
+        {
+            RuleFor(x => x.Password)
+                .NotNull()
+                .NotEmpty()
+                .IsPassword();
+        }
+    }
+
+
+}
